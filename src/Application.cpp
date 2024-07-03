96937af6aca0423445b203fef5df53cabee0cb6b
Spartkaktus <tomikg12345@gmail.com>
2024-07-03 18:50:11 +0200

    layout abstracion

--- src/Application.cpp
+++ src/Application.cpp
@@ -10,7 +10,10 @@
 
 #include "VertexBuffer.h"
 #include "IndexBuffer.h"
+#include "VertexArray.h"
 
+
+//TODO: fix errors cause i have triangle instead of rectangle
 
 struct ShaderProgramSource // struct exists only to be able to return multiple types in function
 {
@@ -110,7 +113,6 @@ int main(void)
 
     /* Create a windowed mode window and its OpenGL context */
 
-
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
     {
@@ -142,14 +144,12 @@ int main(void)
             2, 3, 0
         };
 
-        unsigned int vao; //vertex array object
-        GLCall(glGenVertexArrays(1, &vao));
-        GLCall(glBindVertexArray(vao));
-
+        VertexArray va; 
         VertexBuffer vb(positions, 4 * 2 * sizeof(float));
 
-        GLCall(glEnableVertexAttribArray(0));
-        GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0)); //this links buffer with vao
+        VertexBufferLayout layout;
+        layout.Push<float>(2);
+        va.AddBuffer(vb, layout);
 
         IndexBuffer ib(indicies, 6);
 
@@ -161,8 +161,7 @@ int main(void)
         ASSERT(location != -1);
         GLCall(glUniform4f(location, 0.8f, 0.3f, 0.8f, 1.0f));
 
-        //unbound everything
-        GLCall(glBindVertexArray(0));
+        va.Unbind();
         GLCall(glUseProgram(0));
         GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
         GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
@@ -179,7 +178,7 @@ int main(void)
             GLCall(glUseProgram(shader));
             GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
 
-            GLCall(glBindVertexArray(vao));
+            va.Bind();
             ib.Bind();
 
 
