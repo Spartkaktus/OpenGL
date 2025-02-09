879cbb69a77ba7fa1d5439106d9fd59fbf10bd76
Spartkaktus <tomikg12345@gmail.com>
2025-02-09 17:48:41 +0100

    created test

--- src/Application.cpp
+++ src/Application.cpp
@@ -28,9 +28,6 @@ int main(void)
 {
 	GLFWwindow* window;
 
-	/* Initialize the library */
-
-
 	if (!glfwInit())
 		return -1;
 
@@ -41,9 +38,6 @@ int main(void)
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
 
 
-
-	/* Create a windowed mode window and its OpenGL context */
-
 	window = glfwCreateWindow(960, 540, "Hello World", NULL, NULL);
 	if (!window)
 	{
@@ -51,7 +45,6 @@ int main(void)
 		return -1;
 	}
 
-	/* Make the window's context current */
 
 	glfwMakeContextCurrent(window);
 
@@ -62,50 +55,10 @@ int main(void)
 
 	std::cout << glGetString(GL_VERSION) << std::endl;
 	{
-		/*
-		float positions[] = {
-			-50.0f, -50.0f, 0.0f, 0.0f,   // index 0
-			 50.0f, -50.5f, 1.0f, 0.0f,  // index 1
-			 50.0f,  50.0f, 1.0f, 1.0f,  // index 2
-			-50.0f,  50.0f, 0.0f, 1.0f    // index 3
-		};
 
-		unsigned int indices[] = { //that's the index buffer
-			0, 1, 2,
-			2, 3, 0
-		};
-		*/
 		GLCall(glEnable(GL_BLEND));
 		GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
-		/*
-		VertexArray va; 
-		VertexBuffer vb(positions, 4 * 4 * sizeof(float));
 
-		VertexBufferLayout layout;
-		layout.Push<float>(2);
-		layout.Push<float>(2);
-		va.AddBuffer(vb, layout);
-
-		IndexBuffer ib(indices, 6);
-
-		glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
-
-		Shader shader("res/shaders/Basic.shader");
-		shader.Bind();
-		shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
-
-
-		Texture texture("res/textures/kuoka.png");
-		texture.Bind();
-		shader.SetUniform1i("u_Texture", 0);
-
-
-		va.Unbind();
-		vb.Unbind();
-		ib.Unbind();
-		shader.Unbind();
-		*/
 
 		Renderer renderer;
 
@@ -113,64 +66,41 @@ int main(void)
 		ImGui_ImplGlfwGL3_Init(window, true);
 		ImGui::StyleColorsDark();
 
-		test::TestClearColor test;
+		test::Test* currentTest = nullptr;
+		test::TestMenu* testMenu = new test::TestMenu(currentTest);
+		currentTest = testMenu;
 
-		/*
-		glm::vec3 translationA(200, 200, 0);
-		glm::vec3 translationB(400, 200, 0);
+		testMenu->RegisterTest<test::TestClearColor>("Clear Color");
 
-		float r = 0.0f;
-		float increment = 0.05f;
-
-		*/
 		while (!glfwWindowShouldClose(window))
 		{
+			GLCall(glClearColor(0.0f, 0.0f, 0.0f, 1.0f));
 			renderer.Clear();
 
-			test.OnUpdate(0.0f);
-			test.OnRender();
-
 			ImGui_ImplGlfwGL3_NewFrame();
-			test.OnImGuiRender();
-/*
+			if (currentTest)
 			{
-				glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
-				glm::mat4 mvp = proj * view * model;
-				shader.Bind();
-				shader.SetUniformMat4f("u_MVP", mvp);
-
-				renderer.Draw(va, ib, shader);
+				currentTest->OnUpdate(0.0f);
+				currentTest->OnRender();
+				ImGui::Begin("Test");
+				if (currentTest != testMenu && ImGui::Button("<-"))
+				{
+					delete currentTest;
+					currentTest = testMenu;
+				}
+				currentTest->OnImGuiRender();
+				ImGui::End();
 			}
 
-
-			{
-				glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
-				glm::mat4 mvp = proj * view * model;
-				shader.Bind();
-				shader.SetUniformMat4f("u_MVP", mvp);
-
-				renderer.Draw(va, ib, shader);
-			}
-
-			if (r > 1.0f)
-				increment = -0.05f;
-			else if (r < 0.0f)
-				increment = 0.05f;
-
-			r += increment;
-
-			{
-				ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 960.0f);
-				ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.0f);
-				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-			}
-			*/
 			ImGui::Render();
 			ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
 
 			glfwSwapBuffers(window);
 			glfwPollEvents();
 		}
+		delete currentTest;
+		if (currentTest != testMenu)
+			delete testMenu;
 	}
 
 	ImGui_ImplGlfwGL3_Shutdown();
