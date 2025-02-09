4d94f6aa9eded13f91bda510894abf8e557941f8
Spartkaktus <tomikg12345@gmail.com>
2025-02-09 16:43:40 +0100

    set test

--- src/Application.cpp
+++ src/Application.cpp
@@ -22,6 +22,8 @@
 #include "imgui/imgui.h"
 #include "imgui/imgui_impl_glfw_gl3.h"
 
+#include "tests/TestClearColor.h"
+
 int main(void)
 {
 	GLFWwindow* window;
@@ -60,6 +62,7 @@ int main(void)
 
 	std::cout << glGetString(GL_VERSION) << std::endl;
 	{
+		/*
 		float positions[] = {
 			-50.0f, -50.0f, 0.0f, 0.0f,   // index 0
 			 50.0f, -50.5f, 1.0f, 0.0f,  // index 1
@@ -71,10 +74,10 @@ int main(void)
 			0, 1, 2,
 			2, 3, 0
 		};
-
+		*/
 		GLCall(glEnable(GL_BLEND));
 		GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
-
+		/*
 		VertexArray va; 
 		VertexBuffer vb(positions, 4 * 4 * sizeof(float));
 
@@ -102,6 +105,7 @@ int main(void)
 		vb.Unbind();
 		ib.Unbind();
 		shader.Unbind();
+		*/
 
 		Renderer renderer;
 
@@ -109,20 +113,26 @@ int main(void)
 		ImGui_ImplGlfwGL3_Init(window, true);
 		ImGui::StyleColorsDark();
 
+		test::TestClearColor test;
+
+		/*
 		glm::vec3 translationA(200, 200, 0);
 		glm::vec3 translationB(400, 200, 0);
 
 		float r = 0.0f;
 		float increment = 0.05f;
-		/* loop until the user closes the window */
+
+		*/
 		while (!glfwWindowShouldClose(window))
 		{
-			/* render here */
 			renderer.Clear();
 
+			test.OnUpdate(0.0f);
+			test.OnRender();
+
 			ImGui_ImplGlfwGL3_NewFrame();
-
-
+			test.OnImGuiRender();
+/*
 			{
 				glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
 				glm::mat4 mvp = proj * view * model;
@@ -154,13 +164,11 @@ int main(void)
 				ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.0f);
 				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 			}
+			*/
 			ImGui::Render();
 			ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
 
-			/* swap front and back buffers */
 			glfwSwapBuffers(window);
-
-			/* poll for and process events */
 			glfwPollEvents();
 		}
 	}
