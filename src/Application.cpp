c1f8f98b04a23907b46762910454bc647d6dec40
Spartkaktus <tomikg12345@gmail.com>
2025-02-09 02:03:12 +0100

    Added new model rendered

--- src/Application.cpp
+++ src/Application.cpp
@@ -24,139 +24,151 @@
 
 int main(void)
 {
-    GLFWwindow* window;
+	GLFWwindow* window;
 
-    /* Initialize the library */
-
-
-    if (!glfwInit())
-        return -1;
+	/* Initialize the library */
 
 
-    //specify core profile, in this case 3.3
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);      
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
+	if (!glfwInit())
+		return -1;
+
+
+	//specify core profile, in this case 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);      
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
 
 
 
-    /* Create a windowed mode window and its OpenGL context */
+	/* Create a windowed mode window and its OpenGL context */
 
-    window = glfwCreateWindow(960, 540, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
+	window = glfwCreateWindow(960, 540, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
 
-    /* Make the window's context current */
+	/* Make the window's context current */
 
-    glfwMakeContextCurrent(window);
+	glfwMakeContextCurrent(window);
 
-    glfwSwapInterval(1); //set the vsync to refresh color with the speed of refresh rate
+	glfwSwapInterval(1); //set the vsync to refresh color with the speed of refresh rate
 
-    if (glewInit() != GLEW_OK())
-        std::cout << "Error" << std::endl;
+	if (glewInit() != GLEW_OK())
+		std::cout << "Error" << std::endl;
 
-    std::cout << glGetString(GL_VERSION) << std::endl;
-    {
-        float positions[] = {
-            100.0f, 100.0f, 0.0f, 0.0f,   // index 0
-        	200.0f, 100.5f, 1.0f, 0.0f,  // index 1
-        	200.0f, 200.0f, 1.0f, 1.0f,  // index 2
-            100.0f, 200.0f, 0.0f, 1.0f    // index 3
-        };
+	std::cout << glGetString(GL_VERSION) << std::endl;
+	{
+		float positions[] = {
+			-50.0f, -50.0f, 0.0f, 0.0f,   // index 0
+			 50.0f, -50.5f, 1.0f, 0.0f,  // index 1
+			 50.0f,  50.0f, 1.0f, 1.0f,  // index 2
+			-50.0f,  50.0f, 0.0f, 1.0f    // index 3
+		};
 
-        unsigned int indices[] = { //that's the index buffer
-            0, 1, 2,
-            2, 3, 0
-        };
+		unsigned int indices[] = { //that's the index buffer
+			0, 1, 2,
+			2, 3, 0
+		};
 
-        GLCall(glEnable(GL_BLEND));
-        GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+		GLCall(glEnable(GL_BLEND));
+		GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
 
-        VertexArray va; 
-        VertexBuffer vb(positions, 4 * 4 * sizeof(float));
+		VertexArray va; 
+		VertexBuffer vb(positions, 4 * 4 * sizeof(float));
 
-        VertexBufferLayout layout;
-        layout.Push<float>(2);
-        layout.Push<float>(2);
-        va.AddBuffer(vb, layout);
+		VertexBufferLayout layout;
+		layout.Push<float>(2);
+		layout.Push<float>(2);
+		va.AddBuffer(vb, layout);
 
-        IndexBuffer ib(indices, 6);
+		IndexBuffer ib(indices, 6);
 
-        glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+		glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
 
-        Shader shader("res/shaders/Basic.shader");
-        shader.Bind();
-        shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
-
-
-        Texture texture("res/textures/kuoka.png");
-        texture.Bind();
-        shader.SetUniform1i("u_Texture", 0);
+		Shader shader("res/shaders/Basic.shader");
+		shader.Bind();
+		shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
 
 
-        va.Unbind();
-        vb.Unbind();
-        ib.Unbind();
-        shader.Unbind();
-
-        Renderer renderer;
-
-        ImGui::CreateContext();
-        ImGui_ImplGlfwGL3_Init(window, true);
-        ImGui::StyleColorsDark();
-
-        glm::vec3 translation(200, 200, 0);
-
-        float r = 0.0f;
-        float increment = 0.05f;
-        /* loop until the user closes the window */
-        while (!glfwWindowShouldClose(window))
-        {
-            /* render here */
-            renderer.Clear();
-
-            ImGui_ImplGlfwGL3_NewFrame();
-
-            glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
-            glm::mat4 mvp = proj * view * model;
-
-            shader.Bind();
-            shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
-            shader.SetUniformMat4f("u_MVP", mvp);
+		Texture texture("res/textures/kuoka.png");
+		texture.Bind();
+		shader.SetUniform1i("u_Texture", 0);
 
 
-            renderer.Draw(va, ib, shader);
+		va.Unbind();
+		vb.Unbind();
+		ib.Unbind();
+		shader.Unbind();
 
-            if (r > 1.0f)
-                increment = -0.05f;
-            else if (r < 0.0f)
-                increment = 0.05f;
+		Renderer renderer;
 
-            r += increment;
+		ImGui::CreateContext();
+		ImGui_ImplGlfwGL3_Init(window, true);
+		ImGui::StyleColorsDark();
 
-            {
-                ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 960.0f);
-                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-            }
-            ImGui::Render();
-            ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
+		glm::vec3 translationA(200, 200, 0);
+		glm::vec3 translationB(400, 200, 0);
 
-            /* swap front and back buffers */
-            glfwSwapBuffers(window);
+		float r = 0.0f;
+		float increment = 0.05f;
+		/* loop until the user closes the window */
+		while (!glfwWindowShouldClose(window))
+		{
+			/* render here */
+			renderer.Clear();
 
-            /* poll for and process events */
-            glfwPollEvents();
-        }
-    }
+			ImGui_ImplGlfwGL3_NewFrame();
 
-    ImGui_ImplGlfwGL3_Shutdown();
-    ImGui::DestroyContext();
-    glfwTerminate();
-    return 0;
+
+			{
+				glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
+				glm::mat4 mvp = proj * view * model;
+				shader.Bind();
+				shader.SetUniformMat4f("u_MVP", mvp);
+
+				renderer.Draw(va, ib, shader);
+			}
+
+
+			{
+				glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
+				glm::mat4 mvp = proj * view * model;
+				shader.Bind();
+				shader.SetUniformMat4f("u_MVP", mvp);
+
+				renderer.Draw(va, ib, shader);
+			}
+
+			if (r > 1.0f)
+				increment = -0.05f;
+			else if (r < 0.0f)
+				increment = 0.05f;
+
+			r += increment;
+
+			{
+				ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 960.0f);
+				ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.0f);
+				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+			}
+			ImGui::Render();
+			ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
+
+			/* swap front and back buffers */
+			glfwSwapBuffers(window);
+
+			/* poll for and process events */
+			glfwPollEvents();
+		}
+	}
+
+	ImGui_ImplGlfwGL3_Shutdown();
+	ImGui::DestroyContext();
+	glfwTerminate();
+	return 0;
 }
 
 
